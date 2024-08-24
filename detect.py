7a8e9c44bbe7525303bcc50f23c2a2a861737c8e
Rohit Mahant <138305306+Semicile17@users.noreply.github.com>
2024-08-24 23:00:08 +0530

    Update detect.py

--- detect.py
+++ detect.py
@@ -1,7 +1,7 @@
 import cv2
 from ultralytics import YOLO
 
-# Load the YOLO models
+
 vehicle_model = YOLO("E:/UPES/Traffic/vehicle.pt", task="detect")  # Vehicle detection model
 ambulance_model = YOLO("E:/UPES/Traffic/ambulance.pt", task="detect")  # Ambulance detection model
 
@@ -18,11 +18,11 @@ def draw_boxes(detections, frame, label, color):
             cv2.rectangle(frame, (x1, y1), (x2, y2), color, 2)
             cv2.putText(frame, f'{label} {confidence:.2f}', (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, color, 2)
 
-# Open video capture (0 is for webcam or replace with video path for file input)
-video_path = "E:/UPES/Traffic/video1.mp4"  # Replace with your video path
+
+video_path = "E:/UPES/Traffic/video1.mp4" ]
 cap = cv2.VideoCapture(video_path)
 
-# Check if video is opened successfully
+
 if not cap.isOpened():
     print(f"Error: Unable to open video from path: {video_path}")
     exit()
@@ -53,10 +53,10 @@ while True:
     # Display the frame with detections
     cv2.imshow('Ambulance and Vehicle Detection', frame)
 
-    # Exit on 'q' key press
+
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# Release video capture object and close windows
+
 cap.release()
 cv2.destroyAllWindows()
